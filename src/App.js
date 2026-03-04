1f1bbc6e1e0bd0356a1eae45e15c2f212f0fb407
Mounira Labarang <148859892+Mouniiira@users.noreply.github.com>
2026-03-04 11:57:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Welcome from "./pages/Welcome";
 import Dashboard from "./pages/Dashboard";
 import Journal from "./pages/Journal";
 import EntryPage from "./pages/EntryPage";
+import PhotoBooth from "./pages/PhotoBooth";
+import StyleEditor from "./pages/StyleEditor";
 
 function App() {
   return (
@@ -12,6 +14,9 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/journal" element={<Journal />} />
         <Route path="/entry/:id" element={<EntryPage />} /> {/* ✅ here */}
+        <Route path="/photo" element={<PhotoBooth />} />
+        <Route path="/photo/new" element={<StyleEditor mode="new" />} />
+        <Route path="/photo/:id" element={<StyleEditor mode="edit" />} />
       </Routes>
     </Router>
   );
