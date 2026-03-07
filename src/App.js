a89b42f2438fa86fd4b56ce754da0a9d2928bf63
Mounira Labarang <148859892+Mouniiira@users.noreply.github.com>
2026-03-07 10:57:11 +0100

    Update App.js
    Adding calendar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Journal from "./pages/Journal";
 import EntryPage from "./pages/EntryPage";
 import PhotoBooth from "./pages/PhotoBooth";
 import StyleEditor from "./pages/StyleEditor";
+import CalendarComponent from "./pages/CalendarComponent";
 
 function App() {
   return (
@@ -13,10 +14,11 @@ function App() {
         <Route path="/" element={<Welcome />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/journal" element={<Journal />} />
-        <Route path="/entry/:id" element={<EntryPage />} /> {/* ✅ here */}
+        <Route path="/entry/:id" element={<EntryPage />} />
         <Route path="/photo" element={<PhotoBooth />} />
         <Route path="/photo/new" element={<StyleEditor mode="new" />} />
         <Route path="/photo/:id" element={<StyleEditor mode="edit" />} />
+        <Route path="/calendar" element={<CalendarComponent />} />
       </Routes>
     </Router>
   );
