db12fe31c10ee5c262a2a4e4804cf4d85cbff3dd
Mounira Labarang <148859892+Mouniiira@users.noreply.github.com>
2026-03-26 12:08:42 +0100

    Update ConfirmDialog.js
    Adding "Ok" option to pop up buttons

--- src/components/ConfirmDialog.js
+++ src/components/ConfirmDialog.js
@@ -1,19 +1,26 @@
 import "./ConfirmDialog.css";
 
-function ConfirmDialog({ text, onYes, onNo }) {
+function ConfirmDialog({ text, onYes, onNo, mode = "confirm" }) {
   return (
-    <div className="confirm-overlay">
-      <div className="confirm-box">
-        <p className="confirm-text">{text}</p>
+    <div className="cd-overlay">
+      <div className="cd-box">
+        <p className="cd-text">{text}</p>
 
-        <div className="confirm-buttons">
-          <button className="confirm-btn yes-btn" onClick={onYes}>
-            Yes
-          </button>
-
-          <button className="confirm-btn no-btn" onClick={onNo}>
-            No
-          </button>
+        <div className="cd-buttons">
+          {mode === "confirm" ? (
+            <>
+              <button className="cd-btn yes" onClick={onYes}>
+                Yes
+              </button>
+              <button className="cd-btn no" onClick={onNo}>
+                No
+              </button>
+            </>
+          ) : (
+            <button className="cd-btn ok" onClick={onYes}>
+              OK
+            </button>
+          )}
         </div>
       </div>
     </div>
